c6128ca7e65fe4be0c1b8fc3880182bbc9780472
unknown <mcginty.dom@gmail.com>
2019-12-19 21:51:55 +0000

    views update

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -3,37 +3,39 @@
 @section('title', 'Edit your Comment')
 
 @section('content')
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-      <h1>Edit your Comment</h1>
-      <form method="POST" action="{{ route('comments.update', $comment->id) }}" enctype="multipart/form-data">
-        @csrf
-        @method('PATCH')
-        <p>Body: <input type="text" name="body"
-          value="{{ old('body') }}"></p>
-        <p>Commenting as:
-          <select name="user_id">
-              <option value="{{ Auth::id() }}"
-                @if (Auth::id() == old('user_id'))
-                  selected="selected"
-                @endif
-                >{{ $comment->user->name }}</option>
-          </select>
-        </p>
-        <p>Post ID:
-          <select name="post_id">
-              <option value="{{ $comment->post_id }}"
-                @if ($comment->post_id == old('post_id'))
-                  selected="selected"
-                @endif
-                >{{ $comment->post_id }}</option>
-          </select>
-        </p>
-        <input type="submit" value="Submit">
-        <p><a class="btn btn-primary btn-lg"
-          href="{{ route('posts.show', ['id' => $comment->post_id]) }}" role="button">
-          Return </a></p>
-      </form>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <h1>Edit your Comment</h1>
+        <form method="POST" action="{{ route('comments.update', $comment->id) }}" enctype="multipart/form-data">
+          @csrf
+          @method('PATCH')
+          <p>Body: <input type="text" name="body"
+            value="{{ old('body') }}"></p>
+          <h3>Commenting as:
+            <select name="user_id">
+                <option value="{{ Auth::id() }}"
+                  @if (Auth::id() == old('user_id'))
+                    selected="selected"
+                  @endif
+                  >{{ $comment->user->name }}</option>
+            </select>
+          </h3>
+          <p>Post ID:
+            <select name="post_id">
+                <option value="{{ $comment->post_id }}"
+                  @if ($comment->post_id == old('post_id'))
+                    selected="selected"
+                  @endif
+                  >{{ $comment->post_id }}</option>
+            </select>
+          </p>
+          <input type="submit" value="Submit">
+          <p><a class="btn btn-primary btn-lg"
+            href="{{ route('posts.show', ['id' => $comment->post_id]) }}" role="button">
+            Return </a></p>
+        </form>
+      </div>
     </div>
   </div>
 @endsection
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,25 +3,34 @@
 @section('title', 'Create a Post')
 
 @section('content')
-  <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
-    @csrf
-    <p>Header: <input type="text" name="header"
-      value="{{ old('header') }}"></p>
-    <p>Body: <input type="text" name="body"
-      value="{{ old('body') }}"></p>
-    <p>User ID:
-    <select name="user_id">
-        <option value="{{ $user->id}}"
-          @if ($user->id == old('user_id'))
-            selected="selected"
-          @endif
-          >{{ $user->name }}</option>
-    </select>
-    </p>
-    <p>Upload an image:
-      <input type="file" name="featured_image"
-      value="{{ old('image')}}"> </p>
-    <input type="submit" value="Submit">
-    <a href="{{ route('posts.index') }}"> Return </a>
-  </form>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <h1>Create a Post</h1>
+        <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+          @csrf
+          <h2>Header: <input type="text" name="header"
+            value="{{ old('header') }}"></h2>
+          <p>Body: <input type="text" name="body"
+            value="{{ old('body') }}"></p>
+          <h3>Posting as:
+          <select name="user_id">
+              <option value="{{ $user->id}}"
+                @if ($user->id == old('user_id'))
+                  selected="selected"
+                @endif
+                >{{ $user->name }}</option>
+          </select>
+          </h3>
+          <p>Upload an image:
+            <input type="file" name="featured_image"
+            value="{{ old('image')}}"> </p>
+          <input type="submit" value="Submit">
+          <p><a class="btn btn-primary btn-lg"
+            href="{{ route('posts.index') }}" role="button">
+            Return </a></p>
+        </form>
+      </div>
+    </div>
+  </div>
 @endsection
